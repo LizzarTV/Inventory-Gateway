2ec874def4d6f18ff9258b33a08e6fdb93f16969
Thomas Kortyka <info@toxictoast.de>
2021-05-20 21:07:04 +0200

    test dapr

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Param} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post} from "@nestjs/common";
 import {CategoryService} from "./category.service";
 
 @Controller('categories')
