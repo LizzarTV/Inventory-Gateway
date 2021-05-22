d1880777e3671d315c2bb5c472723d688132dca1
Thomas Kortyka <info@toxictoast.de>
2021-05-23 00:51:35 +0200

    update category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -9,35 +9,35 @@ export class CategoryController {
     @Get()
     getCategoryList(): any {
        this.service.getCategories().then(data => {
-           Logger.debug(data, 'CategoryController');
+           Logger.debug(data.body, 'CategoryController');
        })
     }
 
     @Get('/:id')
     getCategory(@Param('id') id: string): any {
         this.service.getCategory(id).then(data => {
-            Logger.debug(data, 'CategoryController');
+            Logger.debug(data.body, 'CategoryController');
         })
     }
 
     @Post()
     createCategory(@Body() body: { title: string }): any {
         this.service.createCategory(body.title).then(data => {
-            Logger.debug(data, 'CategoryController');
+            Logger.debug(data.body, 'CategoryController');
         })
     }
 
     @Put('/:id')
     updateCategory(@Param('id') id: string, @Body() body: { title: Optional<string>, active: Optional<boolean>}): any {
         this.service.updateCategory(id, body.title, body.active).then(data => {
-            Logger.debug(data, 'CategoryController');
+            Logger.debug(data.body, 'CategoryController');
         })
     }
 
     @Delete('/:id')
     deleteCategory(@Param('id') id: string): any {
         this.service.deleteCategory(id).then(data => {
-            Logger.debug(data, 'CategoryController');
+            Logger.debug(data.body, 'CategoryController');
         })
     }
 }
