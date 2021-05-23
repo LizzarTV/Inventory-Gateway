5313615bcd9fa38295c712ed8e33ee59010fc1ea
Thomas Kortyka <info@toxictoast.de>
2021-05-23 09:49:08 +0200

    add company service

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,9 +1,10 @@
 import {Module} from "@nestjs/common";
 import {AmqpModule} from "../amqp.module";
+import {CompanyService} from "./company.service";
 
 @Module({
     imports: [AmqpModule],
     controllers: [],
-    providers: [],
+    providers: [CompanyService],
 })
 export class CompanyModule {}
