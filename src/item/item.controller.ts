ad458c0bc51fb355e9aea649cc20a74e80975869
Thomas Kortyka <info@toxictoast.de>
2021-05-23 09:24:43 +0200

    update item-controller and module

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,5 +1,6 @@
-import {Body, Controller, Get, Param, Post} from "@nestjs/common";
+import {Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put} from "@nestjs/common";
 import {ItemService} from "./item.service";
+import {Optional} from "../utils/baseTypes.util";
 
 @Controller('item')
 export class ItemController {
@@ -7,17 +8,36 @@ export class ItemController {
     }
 
     @Get()
-    getItemList(): any {
-        return this.service.getItems();
+    getItemList(): Promise<unknown> {
+        return this.service.getItems().catch(error => this.onError(error.code, error.message));
     }
 
     @Get('/:id')
-    getItem(@Param('id') id: string): any {
-        return this.service.getItem(id);
+    getItem(@Param('id') id: string): Promise<unknown> {
+        return this.service.getItem(id).catch(error => this.onError(error.code, error.message));
     }
 
     @Post()
-    createItem(@Body() body: { title: string }): any {
-        return this.service.createItem(body.title);
+    createItem(@Body() body: { title: string }): Promise<unknown> {
+        return this.service.createItem(body.title).catch(error => this.onError(error.code, error.message));
+    }
+
+    @Put('/:id')
+    updateItem(@Param('id') id: string, @Body() body: { title: Optional<string>, active: Optional<boolean>}): Promise<unknown> {
+        return this.service.updateItem(id, body.title, body.active).catch(error => this.onError(error.code, error.message));
+    }
+
+    @Delete('/:id')
+    deleteItem(@Param('id') id: string): Promise<unknown> {
+        return this.service.deleteItem(id).catch(error => this.onError(error.code, error.message));
+    }
+
+    @Put('/:id/restore')
+    restoreItem(@Param('id') id: string): Promise<unknown> {
+        return this.service.restoreItem(id).catch(error => this.onError(error.code, error.message));
+    }
+
+    private onError(code: number, message: string): void {
+        throw new HttpException(message, code || HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,9 +1,10 @@
 import {HttpModule, Module} from "@nestjs/common";
 import {ItemService} from "./item.service";
 import {ItemController} from "./item.controller";
+import {AmqpModule} from "../amqp.module";
 
 @Module({
-    imports: [HttpModule],
+    imports: [AmqpModule],
     controllers: [ItemController],
     providers: [ItemService],
 })
