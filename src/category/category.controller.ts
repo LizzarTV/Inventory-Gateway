30856a6c4a3e816fe8b3e22ac70f839f265625f9
Thomas Kortyka <info@toxictoast.de>
2021-05-23 06:50:40 +0200

    add Return Type

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -7,25 +7,29 @@ export class CategoryController {
     constructor(private readonly service: CategoryService) {}
 
     @Get()
-    getCategoryList(): any {
+    getCategoryList(): Promise<unknown> {
         return this.service.getCategories().catch(error => this.onError(error.code, error.message));
     }
 
     @Get('/:id')
-    getCategory(@Param('id') id: string): any {
+    getCategory(@Param('id') id: string): Promise<unknown> {
         return this.service.getCategory(id).catch(error => this.onError(error.code, error.message));
     }
 
     @Post()
-    createCategory(@Body() body: { title: string }): any {
+    createCategory(@Body() body: { title: string }): Promise<unknown> {
         return this.service.createCategory(body.title).catch(error => this.onError(error.code, error.message));
     }
 
     @Put('/:id')
-    updateCategory(@Param('id') id: string, @Body() body: { title: Optional<string>, active: Optional<boolean>}): any {  }
+    updateCategory(@Param('id') id: string, @Body() body: { title: Optional<string>, active: Optional<boolean>}): Promise<unknown> {
+        return this.service.updateCategory(id, body.title, body.active).catch(error => this.onError(error.code, error.message));
+    }
 
     @Delete('/:id')
-    deleteCategory(@Param('id') id: string): any { }
+    deleteCategory(@Param('id') id: string): Promise<unknown> {
+        return this.service.deleteCategory(id).catch(error => this.onError(error.code, error.message));
+    }
 
     private onError(code: number, message: string): void {
         throw new HttpException(message, code);
