64e71d0b88a25f0bbc24dad08e1cebc9d5c193ab
Thomas Kortyka <info@toxictoast.de>
2021-05-23 10:14:16 +0200

    add size controller

--- src/size/size.module.ts
+++ src/size/size.module.ts
@@ -1,10 +1,11 @@
 import {Module} from "@nestjs/common";
 import {AmqpModule} from "../amqp.module";
 import {SizeService} from "./size.service";
+import {SizeController} from "./size.controller";
 
 @Module({
     imports: [AmqpModule],
-    controllers: [],
+    controllers: [SizeController],
     providers: [SizeService],
 })
 export class SizeModule {}
