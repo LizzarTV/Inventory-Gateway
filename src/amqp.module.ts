c8f321184f7c339dc19c90e2fe37a89299dd11b1
Thomas Kortyka <info@toxictoast.de>
2021-05-23 05:56:57 +0200

    amqp service

--- src/amqp.module.ts
+++ src/amqp.module.ts
@@ -1,28 +1,17 @@
 import {Logger, Module} from "@nestjs/common";
 import {ConfigService} from "@nestjs/config";
-import {RmqUrl} from "@nestjs/microservices/external/rmq-url.interface";
 import {ClientProxyFactory, RmqOptions, Transport} from "@nestjs/microservices";
 
 const amqpService = {
     provide: 'AMQP_SERVICE',
     useFactory: (configService: ConfigService) => {
-        const host = configService.get<string>('AMQP_HOST', 'localhost');
-        const port = configService.get<number>('AMQP_PORT', 5672);
-        const user = configService.get<string>('AMQP_USERNAME', 'guest');
-        const password = configService.get<string>('AMQP_PASSWORD', 'guest');
+        const connectionString = configService.get<string>('AMQP_CONNECTION_STRING', 'amqp://localhost:5672/');
         const queue = configService.get<string>('AMQP_QUEUE', 'queue');
         //
-        const rmqUrl = {
-            protocol: 'amqp',
-            hostname: host,
-            port,
-            username: user,
-            password,
-        } as RmqUrl;
         const clientOptions = {
             transport: Transport.RMQ,
             options: {
-                urls: [rmqUrl],
+                urls: [connectionString],
                 queue,
                 queueOptions: {
                     durable: false
@@ -30,8 +19,6 @@ const amqpService = {
                 noAck: false,
             },
         } as RmqOptions;
-        Logger.debug(rmqUrl, 'RMQURL');
-        Logger.debug(clientOptions, 'Options');
         //
         return ClientProxyFactory.create(clientOptions)
     },
