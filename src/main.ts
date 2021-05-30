c0a58ae7a84a83726874841504f94cd7a3db83d9
Thomas Kortyka <info@toxictoast.de>
2021-05-30 16:28:01 +0200

    advanced cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
+  await corsConfig(app);
   await app.listen(3000);
 }
+
+async function corsConfig(app: INestApplication): Promise<void> {
+  const whitelist = ['http://localhost:3000'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+    });
+}
+
 bootstrap();
