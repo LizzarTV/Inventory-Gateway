1414b6be154f466c3f23b62a7e0770070911fc3b
Thomas Kortyka <info@toxictoast.de>
2021-05-30 16:17:57 +0200

    error handling

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -8,32 +8,56 @@ export class CategoryController {
 
     @Get()
     getCategoryList(): Promise<unknown> {
-        return this.service.getCategories().catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.getCategories().catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Get('/:id')
     getCategory(@Param('id') id: string): Promise<unknown> {
-        return this.service.getCategory(id).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.getCategory(id).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Post()
     createCategory(@Body() body: { title: string }): Promise<unknown> {
-        return this.service.createCategory(body.title).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.createCategory(body.title).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Put('/:id')
     updateCategory(@Param('id') id: string, @Body() body: { title: Optional<string>, active: Optional<boolean>}): Promise<unknown> {
-        return this.service.updateCategory(id, body.title, body.active).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.updateCategory(id, body.title, body.active).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Delete('/:id')
     deleteCategory(@Param('id') id: string): Promise<unknown> {
-        return this.service.deleteCategory(id).catch(error => this.onError(error.code, error.message));
+        try {
+             return this.service.deleteCategory(id).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Put('/:id/restore')
     restoreCategory(@Param('id') id: string): Promise<unknown> {
-        return this.service.restoreCategory(id).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.restoreCategory(id).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     private onError(code: number, message: string): void {
--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -9,35 +9,59 @@ export class ItemController {
 
     @Get()
     getItemList(): Promise<unknown> {
-        return this.service.getItems().catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.getItems().catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Get('/:id')
     getItem(@Param('id') id: string): Promise<unknown> {
-        return this.service.getItem(id).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.getItem(id).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Post()
     createItem(@Body() body: { title: string }): Promise<unknown> {
-        return this.service.createItem(body.title).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.createItem(body.title).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Put('/:id')
     updateItem(@Param('id') id: string, @Body() body: { title: Optional<string>, active: Optional<boolean>}): Promise<unknown> {
-        return this.service.updateItem(id, body.title, body.active).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.updateItem(id, body.title, body.active).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Delete('/:id')
     deleteItem(@Param('id') id: string): Promise<unknown> {
-        return this.service.deleteItem(id).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.deleteItem(id).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     @Put('/:id/restore')
     restoreItem(@Param('id') id: string): Promise<unknown> {
-        return this.service.restoreItem(id).catch(error => this.onError(error.code, error.message));
+        try {
+            return this.service.restoreItem(id).catch(error => this.onError(error.code, error.message));
+        } catch (error) {
+            this.onError(error.code, error.message);
+        }
     }
 
     private onError(code: number, message: string): void {
-        throw new HttpException(message, code || HttpStatus.INTERNAL_SERVER_ERROR);
+        throw new HttpException(message || 'Unknown Error', code || HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
